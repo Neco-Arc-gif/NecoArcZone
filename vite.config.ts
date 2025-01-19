f1ed80662a8120189647ea253dedbb533a9d76a3
Neco-Arc-gif <melvyn.saksik@gmail.com>
2025-01-19 19:13:52 +0100

    vite config update

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/NecoArcZone',
+  base: '/NecoArcZone/',
   optimizeDeps: {
     include: ['lucide-react','react', 'react-dom'],
   },
